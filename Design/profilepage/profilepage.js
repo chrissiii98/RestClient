3b76fde580163916dabbb4ec6cddec7ae4e09453
Chrissi Strauss <cStrauss@hof-university.de>
2018-12-10 16:20:00 +0100

    profile seite

--- Design/profilepage/profilepage.js
+++ Design/profilepage/profilepage.js
@@ -1,19 +1,3 @@
-$('#username').on('input', function () {
-  var username = $("#username").val();
-    $('#pwd').attr("placeholder", username);
-    $('#ergebnis').html('<p>'+username+'</p>');
- });
-
-
-$('#test').click(function() {
-    var username = $("#username").val(); 
-    alert(username);
-    if (username != null){
-        alert("test2");
-        $('#username').attr("placeholder", username);    
-    }  
-});
-
 window.onload= function() {
-      $('#username').attr("placeholder", "test");    
+      $('#username').attr("placeholder", "mmustermann");    
 };
