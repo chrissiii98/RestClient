84dba01e0908c21d7f4083a00a8c4f4cbb198bf5
Chrissi Strauss <cStrauss@hof-university.de>
2018-12-12 14:12:47 +0100

    neue vacation erstellen fertig

--- neue pages/js/main.js
+++ neue pages/js/main.js
@@ -7,3 +7,19 @@ $('#arrowUp').click(function() {
     $('#arrowDown').css("visibility","visible");
     $('#hiddenElement').css("display", "none");
 });
+
+
+$(function() {
+  $('input[name="daterange"]').daterangepicker({
+      autoUpdateInput: false,
+      locale: {
+          cancelLabel: 'Clear'
+      }
+  });
+  $('input[name="daterange"]').on('apply.daterangepicker', function(ev, picker) {
+      $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+  });
+  $('input[name="daterange"]').on('cancel.daterangepicker', function(ev, picker) {
+      $(this).val('');
+  });
+});
